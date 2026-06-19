735d41ca9658bdf50e23f3fbe1023078209985dc
Santhosh VS <santhosh02vs@gmail.com>
2026-06-20 01:53:18 +0530

    feat(frontend): add protected and public route guards

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import { Toaster } from "react-hot-toast";
+import PublicRoute from "./components/publicRoute";
+import ProtectedRoute from "./components/protectedRoute";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route element={<PublicRoute />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+
+          <Route element={<ProtectedRoute />}></Route>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
         </Routes>
         <Toaster />
       </BrowserRouter>
